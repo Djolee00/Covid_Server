70988e65213843c4868a8bd7927134e5dfb0a5dc
Djolee00 <ivanovic.djordje2019@gmail.com>
2021-12-13 23:41:25 +0100

    Profile Menu

--- src/main/ServerThread.java
+++ src/main/ServerThread.java
@@ -36,13 +36,13 @@ public class ServerThread extends Thread {
 
 		String clientChoice = null;
 
-		clientOutput.println("***MAIN MENU***" 
-				+ "\n\n1. Register"
+		clientOutput.println("***\nMAIN MENU***" 
+				+ "\n\n1.Register"
 				+ "\n2.Login" 
 				+ "\n3.Login as Administrator"
-				+ "\n\nTo exit application at eny time, please type:" 
+				+ "\n\nTo exit application at any time, please type:" 
 				+ "\n***exit" 
-				+ "\n\nTo get back to main menu, please type:" 
+				+ "\n\nTo get back to main menu at any time, please type:" 
 				+ "\n***return"
 				+ "\n\nYour choice?  ");
 
@@ -97,7 +97,7 @@ public class ServerThread extends Thread {
 		int secondDose = 0;
 		int thirdDose = 0;
 
-		clientOutput.println("\n\n***WELCOME, PLEASE FILL REGISTRAION FORM***");
+		clientOutput.println("\n\n***WELCOME, PLEASE FILL REGISTRATION FORM***");
 
 		try {
 			do {
@@ -369,15 +369,192 @@ public class ServerThread extends Thread {
 			}
 			
 			}while(!valid);
+			
+			
 		} catch (IOException e) {
 			clientOutput.println("Error while getting client input.");
 		}
 		
+		userProfileMenu();
 		
 	}
 	
-	//***VACCINE CHOICE MENU***
+	//***USER PROFILE***
 
+	private void userProfileMenu() {
+		
+		clientOutput.println("\n\n***WELCOME***\n");
+		clientOutput.println("Your personal data:");
+		String format = "%-10s %-15s %-20s %-10s %-35s%-1s";
+		String ulepsavanje="";
+		for(int i =1;i<=94;i++) {
+			ulepsavanje+="=";
+		}
+		clientOutput.println(ulepsavanje+"\n"
+				+String.format(format, "Name:","Surname:","Personal ID:","Gender:","Email:","|")+"\n"+
+				String.format(format, "----","-------","-----------","------","-----","|")+"\n"+
+				String.format(format, user.getName(),user.getSurname(),user.getPersonalID(),user.getGender(),user.getEmail(),"|")+"\n"+
+				ulepsavanje);
+		
+		String format2 = "%-25s%-25s%-25s%-1s";
+		String ulepsavanje2="";
+		for(int i=1;i<=75;i++) {
+			ulepsavanje2+="=";
+		}
+		clientOutput.println(String.format(format2, "First Dose:","Second Dose:","Third Dose:","|")+"\n"
+				+String.format(format2, "----------","-----------","----------","|")+
+				"\n"+String.format(format2,getVaccine(user.getFirstDose()),getVaccine(user.getSecondDose()),getVaccine(user.getThirdDose()),"|")+"\n"+
+				ulepsavanje2);
+	
+		
+		String choice = null;
+		boolean isValid = false;
+		clientOutput.println("1.Change my answers\n"
+				+ "2.Check my green certificate status\n"
+				+ "3.Log out\n"
+				+"Choose:");
+		
+		try {
+			do {
+			if(communicationSocket!=null && !communicationSocket.isClosed())
+				choice = clientInput.readLine();
+			
+			if(isExit(choice)) {
+				return;
+			}
+			
+			if(isReturn(choice)) {
+				clientOutput.println("\n\n");
+				user = null;
+				menu();
+				return;
+			}
+			
+			if(choice!=null)
+				if(choice.equals("1") || choice.equals("2") || choice.equals("3")){
+					isValid = true;
+				}else {
+					clientOutput.println("Invalid input try again");
+				}
+			}while(!isValid);
+			
+		} catch (IOException e) {
+			clientOutput.println("Error while getting client input");
+		}
+		
+		if(choice!=null) {
+			switch (choice) {
+				case "1":
+					answerChangeMenu();
+					userProfileMenu();
+					break;
+				case "2":
+					checkGreenCertificate();
+					userProfileMenu();
+					break;
+				case "3":
+					user=null;
+					menu();
+					return;		
+			}
+		}
+	
+	}
+	//***ANSWER CHANGE MENU***
+	
+	private void answerChangeMenu() {
+		int choice = 0;
+		String secondChoice = null;
+		boolean valid = false;
+		
+		//FIRST QUESTION
+		if(user.getFirstDose() == 0) {
+			clientOutput.println("\n***FIRST DOSE***");
+			
+			choice = vaccineChoiceMenu();
+			if(choice == -1) {
+				return;
+			}
+			if(choice == -2) {
+				clientOutput.println("\n\n");
+				user = null;
+				menu();
+				return;
+			}
+			if(choice == 0) {
+				return;
+			}else {
+				updateFirstDose(choice);
+				user.setFirstDose(choice);
+			}
+		}
+		
+		//SECOND QUESTION
+		if(user.getSecondDose() == 0) {
+			clientOutput.println("\n***SECOND DOSE***");
+			try {
+			do {
+				clientOutput.println("\nHave you been vaccinated with second dose?"
+						+ "\n\n1.Yes\n2.No");
+				clientOutput.println("Answer: ");
+				secondChoice=clientInput.readLine();
+				if(isExit(secondChoice)) {
+					return;
+				}
+				if(isReturn(secondChoice)) {
+					clientOutput.println("\n\n");
+					user = null;
+					menu();
+					return;
+				}
+				
+				if(secondChoice!=null)
+					switch (secondChoice){
+						case "1":
+							valid=true;
+							user.setSecondDose(user.getFirstDose());
+							updateSecondDose();
+							break;
+						case "2":
+							valid = true;
+							break;
+						default:
+							clientOutput.println("Invalid input try again");
+				}
+			}while(!valid);
+			}catch (IOException e) {
+				clientOutput.println("Error while getting client input");
+			}
+		}
+		
+		choice = 0;
+		//THIRD QUESTION
+		if(user.getSecondDose()>0 && user.getThirdDose() == 0) {
+			clientOutput.println("\n***THIRD DOSE***");
+			
+			choice = vaccineChoiceMenu();
+			if(choice == -1) {
+				return;
+			}
+			if(choice == -2) {
+				clientOutput.println("\n\n");
+				user = null;
+				menu();
+				return;
+			}
+			if(choice == 0) {
+				return;
+			}else {
+				updateThirdDose(choice);
+				user.setThirdDose(choice);
+			}
+		}else {
+			clientOutput.println("\nAll questions have been answered");
+		}
+		
+	}
+
+	//***VACCINE CHOICE MENU***
 	private int vaccineChoiceMenu() {
 		clientOutput.println("\n1.Pfizer-BioNTech"
 				+ "\n2.Sputnik V"
@@ -427,6 +604,58 @@ public class ServerThread extends Thread {
 
 
 	//***WORK WITH DATABASE***
+	private void updateThirdDose(int choice) {
+		String sql = "UPDATE korisnici SET trecaDoza=? WHERE username=?";
+		try(PreparedStatement statement = dbConnection.prepareStatement(sql)){
+			statement.setString(2, user.getUsername());
+			statement.setInt(1, choice);
+			
+			int row=statement.executeUpdate();
+			if(row>0) {
+				System.out.println("Third dose successfully updated for user: "+user.getUsername());
+			}else {
+				clientOutput.println("Third dose status could not be updated");
+			}
+		}catch(SQLException e) {
+			clientOutput.println("Error while updating first dose status");
+		}
+	}
+	
+	private void updateSecondDose() {
+		String sql = "UPDATE korisnici SET drugaDoza=? WHERE username=?";
+		try(PreparedStatement statement = dbConnection.prepareStatement(sql)){
+			statement.setString(2, user.getUsername());
+			statement.setInt(1, user.getSecondDose());
+			
+			int row=statement.executeUpdate();
+			if(row>0) {
+				System.out.println("Second dose successfully updated for user: "+user.getUsername());
+			}else {
+				clientOutput.println("Second dose status could not be updated");
+			}
+		}catch(SQLException e) {
+			clientOutput.println("Error while updating first dose status");
+		}
+		
+	}
+	
+	private void updateFirstDose(int choice) {
+		String sql = "UPDATE korisnici SET prvaDoza=? WHERE username=?";
+		try(PreparedStatement statement = dbConnection.prepareStatement(sql)){
+			statement.setString(2, user.getUsername());
+			statement.setInt(1, choice);
+			
+			int row=statement.executeUpdate();
+			if(row>0) {
+				System.out.println("First dose successfully updated for user: "+user.getUsername());
+			}else {
+				clientOutput.println("First dose status could not be updated");
+			}
+		}catch(SQLException e) {
+			clientOutput.println("Error while updating first dose status");
+		}
+		
+	}
 	
 	private void getBackUserFromDatabase(String username, String password) {
 		String query = "SELECT * FROM korisnici WHERE username ='"+username+"' AND password ='"+password+"'";
@@ -522,6 +751,14 @@ public class ServerThread extends Thread {
 	
 
 	// ***CHECK Methods***
+	
+	private void checkGreenCertificate() {
+		if(user.getSecondDose() > 0) {
+			clientOutput.println("\nYou possess valid green certificate");
+		}else {
+			clientOutput.println("\nYou don't possess valid green cerficate, because you didn't get second dose");
+		}
+	}
 
 	private boolean isExit(String s) {
 		if (s == null || s.startsWith("***exit")) {
@@ -533,6 +770,23 @@ public class ServerThread extends Thread {
 	
 	private boolean isReturn(String s) {
 		return s == null || s.startsWith("***return");
+	}
+	
+	//***GET VACCINE***
+	
+	private String getVaccine(int num) {
+		switch(num) {
+		case 1:
+			return "Pfizer-BioNTech";
+		case 2:
+			return "Sputnik V";
+		case 3:
+			return "Sinopharm";
+		case 4: 
+			return "AstraZeneca";
+		default:
+			return "Vaccine not received";
+		}
 	}
 	
 
